fd0fed4f0be56069ec7cf0f6f1ad872f9258c608
616164124@qq.com <616164124@qq.com>
2022-07-31 17:39:05 +0800

    2022年7月31日

---
+++ cloud-common-bean/src/main/java/com/cloudcommonbean/CloudCommonBeanApplication.java
@@ -0,0 +1,13 @@
+package com.cloudcommonbean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudCommonBeanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudCommonBeanApplication.class, args);
+    }
+
+}
---
+++ cloud-common-bean/src/test/java/com/cloudcommonbean/CloudCommonBeanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudcommonbean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudCommonBeanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
